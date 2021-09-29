3e058a67ecd52414bfeb4a6aeffa795240ec9fa1
Márcio Conceição Goulart <marcio.goulart@gmail.comt>
2021-09-29 11:40:03 -0300

    Adds 4 programs as test cases for the parser

--- src/test/scala/br/unb/cic/wlang/parser/WhileProgramParserTest.scala
+++ src/test/scala/br/unb/cic/wlang/parser/WhileProgramParserTest.scala
@@ -127,4 +127,53 @@ class WhileProgramParserTest extends  AnyFunSuite with BeforeAndAfter {
       case p.Error(msg,_) => println(s"ERROR: $msg"); fail
     }
   }
+
+  test("Test for the cube module") {
+    val content = ResourceHandle.getContent("Cube.wp")
+
+    assert(content != null)
+
+    p.parse(p.whileProgram, content) match {
+      case p.Success(program, _) => succeed
+      case p.Failure(msg, _) => println(s"FAILURE: $msg"); fail
+      case p.Error(msg,_) => println(s"ERROR: $msg"); fail
+    }
+  }
+
+  test("Test for the swapvars module") {
+    val content = ResourceHandle.getContent("Swapvars.wp")
+
+    assert(content != null)
+
+    p.parse(p.whileProgram, content) match {
+      case p.Success(program, _) => succeed
+      case p.Failure(msg, _) => println(s"FAILURE: $msg"); fail
+      case p.Error(msg,_) => println(s"ERROR: $msg"); fail
+    }
+  }
+
+  test("Test for the square module") {
+    val content = ResourceHandle.getContent("Square.wp")
+
+    assert(content != null)
+
+    p.parse(p.whileProgram, content) match {
+      case p.Success(program, _) => succeed
+      case p.Failure(msg, _) => println(s"FAILURE: $msg"); fail
+      case p.Error(msg,_) => println(s"ERROR: $msg"); fail
+    }
+  }
+
+  test("Test for the check positive, negative or zero module") {
+    val content = ResourceHandle.getContent("CheckPosNegZero.wp")
+
+    assert(content != null)
+
+    p.parse(p.whileProgram, content) match {
+      case p.Success(program, _) => succeed
+      case p.Failure(msg, _) => println(s"FAILURE: $msg"); fail
+      case p.Error(msg,_) => println(s"ERROR: $msg"); fail
+    }
+  }
+
 }
