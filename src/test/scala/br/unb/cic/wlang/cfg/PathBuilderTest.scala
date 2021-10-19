1556cd69cf3d2939a28bba9f27895137e70333b9
jose clavo tafur <jclavotafur@gmail.com>
2021-10-19 19:57:12 -0300

    more test for path

--- src/test/scala/br/unb/cic/wlang/cfg/PathBuilderTest.scala
+++ src/test/scala/br/unb/cic/wlang/cfg/PathBuilderTest.scala
@@ -9,6 +9,11 @@ class PathBuilderTest extends AnyFunSuite {
 
   val flow1: CFG = Set((1,2), (2,3), (3,4), (4,5), (5,3), (3,6))
 
+  test("Test for path 1 flow1") {
+    val expected : Set[Path] = Set(List(1))
+    assert(expected == path(1, 1, flow1))
+  }
+
   test("Test for path 1 2 flow1") {
     val expected : Set[Path] = Set(List(1,2))
     assert(expected == path(1, 2, flow1))
@@ -17,6 +22,11 @@ class PathBuilderTest extends AnyFunSuite {
   test("Test for path 1 3 flow1") {
     val expected : Set[Path] = Set(List(1,2,3))
     assert(expected == path(1, 3, flow1))
+  }
+
+  test("Test for path 2 3 flow1") {
+    val expected : Set[Path] = Set(List(2,3))
+    assert(expected == path(2, 3, flow1))
   }
 
   test("Test for path 1 4 flow1") {
@@ -33,4 +43,14 @@ class PathBuilderTest extends AnyFunSuite {
     val expected : Set[Path] = Set(List(1,2,3,4,5,3,6), List(1,2,3,6))
     assert(expected == path(1, 6, flow1))
   }
+
+
+  // val flow2: CFG = Set((2,0), (2,1), (0,3), (0,1), (1,3))
+  val flow2: CFG = Set((0,1), (0,3), (1,3), (2,0), (2,1))
+
+  test("Test new") {
+    val expected : Set[Path] = Set(List(2, 1, 3), List(2, 0, 1, 3), List(2, 0, 3))
+    assert(expected == path(2, 3, flow2))
+  }
+
 }
