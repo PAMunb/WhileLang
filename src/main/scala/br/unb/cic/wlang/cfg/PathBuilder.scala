898a3bf58919d7eaeabd0ba2716258908e75e14f
Rodrigo Bonifácio <rbonifacio123@gmail.com>
2021-10-30 06:01:58 -0300

    small refactoring (replacing if-then by pattern matching)

--- src/main/scala/br/unb/cic/wlang/cfg/PathBuilder.scala
+++ src/main/scala/br/unb/cic/wlang/cfg/PathBuilder.scala
@@ -27,31 +27,26 @@ object PathBuilder {
   def completePath(path: Path, flow: CFG, interFlow: InterCFG): Boolean =
     completePath(path, flow, interFlow, new mutable.Stack[Label]())
 
-  def completePath(path: Path, flow: CFG, interFlow: InterCFG, stack: mutable.Stack[Label]): Boolean = {
-    // this is the base cases of the recursion.
-    // in this case, we have recognized a path as being complete
-    if(path.size == 0 && stack.isEmpty)        return true
-    else if (path.size == 0 && !stack.isEmpty) return false
-    else {
-      // here we check if the label is either a call or a return, making
-      // use of the interflow.
-      val callEdge = interFlow.find({case (lc, _, _, _) => lc == path.head })
-      val returnEdge = interFlow.find({case (_, _, _, lr) => lr == path.head })
-      if(callEdge.isDefined) { // if this is a call edge, we must "push" into the stack.
-        val (_, _, _, lr) = callEdge.get
-        return completePath(path.tail, flow, interFlow, stack.push(lr))
+  def completePath(path: Path, flow: CFG, interFlow: InterCFG, stack: mutable.Stack[Label]): Boolean =
+    path match {
+      case List() if stack.isEmpty => true // base case + succeeded to recognize the path
+      case List() if !stack.isEmpty => false // base case + failed to recognize the path
+      case _ => { // the recursive case
+        val callEdge = interFlow.find({ case (lc, _, _, _) => lc == path.head })
+        val returnEdge = interFlow.find({ case (_, _, _, lr) => lr == path.head })
+        if (callEdge.isDefined) { // if this is a call edge, we must "push" into the stack.
+          val (_, _, _, lr) = callEdge.get
+          return completePath(path.tail, flow, interFlow, stack.push(lr))
+        }
+        else if (returnEdge.isDefined) { // if this is a return edge, we must "pop" from the stack (if lr == stack.top)
+          val (_, _, _, lr) = returnEdge.get
+          if (lr == stack.top) {
+            stack.pop() // removes the top element
+            return completePath(path.tail, flow, interFlow, stack)
+          } //if lr != top, we found an incomplete path.
+          else return false
+        } // otherwise, we just continue checking if a path is valid or not.
+        else return completePath(path.tail, flow, interFlow, stack)
       }
-      else if(returnEdge.isDefined) { // if this is a return edge, we must "pop" from the stack (if lr == stack.top)
-        val (_, _, _, lr) = returnEdge.get
-        if(lr == stack.top) {
-          stack.pop() // removes the top element
-          return completePath(path.tail, flow, interFlow, stack)
-        } //if lr != top, we found an incomplete path.
-        else return false
-      } // otherwise, we just continue checking if a path is valid or not.
-      else return completePath(path.tail, flow, interFlow, stack)
     }
-  }
-
-
 }
